f97b530ad2a0f3ec1d41284de18ef797755bf6b4
Mikael Storman Gustavsson <miga02@gmail.com>
2024-10-02 17:38:07 +0200

    Testing preprocessor directives using a conditional symbol from the publishing profile.

---
+++ PreprocessDirective/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+#if MY_DIRECTIVE
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+#else
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Bad World!");
+#endif
+
+Console.ReadLine();
